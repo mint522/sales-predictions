746a1d403d13c14743f686324998f3e3fb2b9550
mint522 <ljmint522@gmail.com>
2021-05-21 21:03:08 -0700

    convert category data

--- sales_predictions.py
+++ sales_predictions.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np 
 import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
@@ -24,32 +23,35 @@ df['Item_Fat_Content'] = df['Item_Fat_Content'].replace({'low fat': 'Low Fat', '
 
 
 #Encoding Categorical Data
-#referece: https://towardsdatascience.com/understanding-feature-engineering-part-2-categorical-data-f54324193e63
-fat_label = LabelEncoder().fit_transform(df['Item_Fat_Content'])
+fat_label = df['Item_Fat_Content'].replace({'Low Fat':0, 'Regular':1})
 df['Item_Fat_Content_Label'] = fat_label
 
-print(df['Item_Type'].value_counts())
-item_type_label = LabelEncoder().fit_transform(df['Item_Type'])
-df['Item_Type_Label'] = item_type_label
-
 print(df['Outlet_Location_Type'].value_counts())
-outlet_location_type_label = LabelEncoder().fit_transform(df['Outlet_Location_Type'])
+outlet_location_type_label = df['Outlet_Location_Type'].replace({'Tier 1':1, 'Tier 2':2, 'Tier 3': 3})
 df['Outlet_Location_Type_Label'] = outlet_location_type_label
 
 print(df['Outlet_Type'].value_counts())
-outlet_type_label = LabelEncoder().fit_transform(df['Outlet_Type'])
+outlet_type_label = df['Outlet_Type'].map({'Grocery Store':0, 'Supermarket Type1':1, 'Supermarket Type2':2, 'Supermarket Type3': 3})
 df['Outlet_Type_Label'] = outlet_type_label
 
-print(df.head(10))
-print(df.info())
+#reference: https://colab.research.google.com/drive/1ocNoCkcyVzC_QipOOCa6kssFhABswEH8
+print(df['Item_Type'].value_counts())
+df_dummies = pd.get_dummies(df, columns = ['Item_Type'], drop_first=True)
+df_analyze = df_dummies.drop(columns=['Item_Identifier', 'Item_Fat_Content', 'Outlet_Identifier', 'Outlet_Location_Type', 'Outlet_Type'])
+print(df_analyze.info())
 
 
 #Linear Regression
-X = df[['Item_Visibility', 'Item_MRP', 'Outlet_Establishment_Year', 'Item_Fat_Content_Label', 'Item_Type_Label', 'Outlet_Location_Type_Label', 'Outlet_Type_Label']]
-y = df['Item_Outlet_Sales']
+X = df_analyze.drop(columns=['Item_Outlet_Sales'])
+y = df_analyze['Item_Outlet_Sales']
+
+scaler = StandardScaler()
+scaler.fit(X)
+X = scaler.transform(X)
 
 reg = LinearRegression(fit_intercept=True)
 reg.fit(X, y)
+print(reg.predict(X))
 #Coefficient of Determination
 print(r2_score(y, reg.predict(X)))
 #Mean squared error (MSE)
@@ -61,9 +63,6 @@ print(np.sqrt(mean_squared_error(y, reg.predict(X))))
 
 
 #KNN
-scaler = StandardScaler()
-scaler.fit(X)
-X = scaler.transform(X)
 knn = KNeighborsRegressor()
 knn.fit(X, y)
 print(knn.predict(X))
@@ -119,15 +118,15 @@ plt.xlabel('Item Fat Content', fontsize=8)
 plt.title('Item Outlet Sales by Item Fat Content', fontsize=10)
 
 #heatmap
-#reference: https://www.geeksforgeeks.org/how-to-change-the-colorbar-size-of-a-seaborn-heatmap-figure-in-python/
+#reference: https://seaborn.pydata.org/generated/seaborn.heatmap.html
 plt.subplot(2,3,6)
-sns.heatmap(df.corr(), annot=None, square=True, cmap='Blues', xticklabels=(['Item Visibility', 'MRP', 'Outlet Est Year', 'Sales', 'Item Fat', 'Item Type', 'Outlet Loc', 'Outlet Type']), yticklabels=(['Item Visibility', 'MRP', 'Outlet Est Year', 'Sales', 'Item Fat', 'Item Type', 'Outlet Loc', 'Outlet Type']))
+sns.heatmap(df_analyze.iloc[:, 0:7].corr(), annot=None, square=True, cmap='YlGnBu', xticklabels=(['Item Visibility', 'Item MRP', 'Outlet Est Year', 'Sales', 'Item Fat', 'Outlet Loc', 'Outlet Type']), yticklabels=(['Item Visibility', 'Item MRP', 'Outlet Est Year', 'Sales', 'Item Fat', 'Outlet Loc', 'Outlet Type']))
 plt.yticks(rotation=0)
-plt.title('Correlation between features', fontsize=10)
+plt.title('Correlation between features')
 plt.show()
 
 #Is there anything you can do to improve your model?
-#Could remove records where 'Item_Visibility'==0 
+#Use get_dummy or one hot encoder on column 'Outlet_Type' can increase the accuracy of linear regression
 
 #Which features are most associated with higher predicted sales?
 #Item Outlet Sales is more related with Item MRP and Outlet Type
